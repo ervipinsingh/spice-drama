234c58723d9a4bd4c24166b5b54f038ae53cb4d0
Atul Verma <atulverma0225@gmail.com>
2026-02-08 15:06:59 +0530

    Fix AuthContext to use adminApi

--- src/Components/ExploreMenu/Exploremenu.jsx
+++ src/Components/ExploreMenu/Exploremenu.jsx
@@ -5,7 +5,7 @@ import "../../App.css";
 const Exploremenu = ({ Category, SetCategory }) => {
   return (
     <div
-      className="bg-gradient-to-b from-orange-50 to-white mt-10 pt-10 px-5 md:mx-10"
+      className="mt-10 pt-10 px-5 md:mx-10"
       id="explore-menu"
     >
       <h1 className="flex justify-center text-2xl sm:text-3xl md:text-4xl">
