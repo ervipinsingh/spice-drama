8189c61c93fcd30c0288af0e3b75b2c40210515e
Vipin SIngh <er.vipinsingh1@gmail.com>
2026-03-23 20:39:19 +0530

    Update sendMail.js

--- Backend/utils/sendMail.js
+++ Backend/utils/sendMail.js
@@ -6,7 +6,7 @@ const sendMail = async (options) => {
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
-    secure: true, // 465 ke liye correct
+    secure: false, // 465 ke liye correct
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
