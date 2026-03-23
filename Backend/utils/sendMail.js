5d5ac624fff278809a32d948843d3717c45df21c
Vipin SIngh <er.vipinsingh1@gmail.com>
2026-03-24 01:11:22 +0530

    Update sendMail.js

--- Backend/utils/sendMail.js
+++ Backend/utils/sendMail.js
@@ -1,13 +1,7 @@
 import nodemailer from "nodemailer";
-import dns from "dns";
-
-// FORCE IPv4
-dns.setDefaultResultOrder("ipv4first");
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
@@ -17,15 +11,14 @@ const transporter = nodemailer.createTransport({
 const sendMail = async (options) => {
   console.log("🚀 sendMail function called", options.email);
 
-  const mailOptions = {
-    from: process.env.SMTP_USER,
-    to: options.email,
-    subject: options.subject,
-    html: options.message,
-  };
-
   try {
-    const info = await transporter.sendMail(mailOptions);
+    const info = await transporter.sendMail({
+      from: "Spice Drama <order@spicedrama.com>",
+      to: options.email,
+      subject: options.subject,
+      html: options.message,
+    });
+
     console.log("📧 Email sent:", info.messageId);
   } catch (error) {
     console.error("❌ EMAIL ERROR:", error);
